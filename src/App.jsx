5a17176e9ea08b975ed91745c26a5d07e12ea9fa
afmsabbir <khanafmsabbir@gmail.com>
2024-03-16 03:13:09 +0600

    Add Bookmark Button And Lift Up The State

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
+import { useState } from "react";
 import "./App.css";
 import Blogs from "./components/Blogs/Blogs";
 import Bookmarks from "./components/Bookmarks/Bookmarks";
 import Header from "./components/Header/Header";
 
 function App() {
+  const [bookmarks, setBookmarks] = useState([]);
+  const handleAddToBookmark = blog =>{
+    console.log('bookmark add soon');
+  }
   return (
-    <>
+    <div className="max-w-7xl mx-auto">
       <Header></Header>
       <div className="md:flex">
-        <Blogs></Blogs>
+        <Blogs handleAddToBookmark ={handleAddToBookmark}></Blogs>
         <Bookmarks></Bookmarks>
       </div>
-    </>
+    </div>
   );
 }
 
--- src/components/Blog/Blog.jsx
+++ src/components/Blog/Blog.jsx
@@ -1,33 +1,51 @@
-import PropTypes from 'prop-types';
-const Blog = ({blog}) => {
-    const {title, cover,reading_time, author, author_img, posted_date, hashtags} = blog;
-    return (
-        <div>
-            <img src={cover} alt= {`cover picture of ${title}`} />
-            <div className='flex justify-between'>
-                <div className='flex'>
-                    <img className='w-14' src={author_img} alt={`author img ${author}`} />
-                    <div className='ml-6'>
-                        <h3 className='text-2xl'>{author}</h3>
-                        <p>{posted_date}</p>
-                    </div>
-                </div>
-                <div>
-                    <span>{reading_time} min read</span>
-                </div>
-            </div>
-            <h2 className='text-4xl font-bold '>{title}</h2>
-            <p>
-                {
-                    hashtags.map((hash, idx) => <span key={idx}><a href="">#{hash}</a></span>)
-                }
-            </p>
+import PropTypes from "prop-types";
+import { CiBookmark } from "react-icons/ci";
+const Blog = ({ blog, handleAddToBookmark }) => {
+  const {
+    title,
+    cover,
+    reading_time,
+    author,
+    author_img,
+    posted_date,
+    hashtags,
+  } = blog;
+  return (
+    <div className="mb-20">
+      <img
+        className="w-full mb-8"
+        src={cover}
+        alt={`cover picture of ${title}`}
+      />
+      <div className="flex justify-between mb-4">
+        <div className="flex">
+          <img className="w-14" src={author_img} alt={`author img ${author}`} />
+          <div className="ml-6">
+            <h3 className="text-2xl">{author}</h3>
+            <p>{posted_date}</p>
+          </div>
         </div>
-    );
+        <div>
+          <span>{reading_time} min read</span>
+          <button onClick={handleAddToBookmark} className="ml-2">
+            <CiBookmark />
+          </button>
+        </div>
+      </div>
+      <h2 className="text-4xl font-bold mb-2">{title}</h2>
+      <p>
+        {hashtags.map((hash, idx) => (
+          <span key={idx}>
+            <a href="">#{hash}</a>
+          </span>
+        ))}
+      </p>
+    </div>
+  );
 };
 
 Blog.propTypes = {
-    blog: PropTypes.object.isRequired
-}
+  blog: PropTypes.object.isRequired,
+};
 
 export default Blog;
